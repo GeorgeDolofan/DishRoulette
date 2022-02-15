c4f34a6cbc4387c3c9d4810f12fe45c590c45bb0
George <dolofan_g@yahoo.com>
2022-02-15 23:01:17 +0200

    First commit

---
+++ app/src/main/java/com/example/dishrouletteapp/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.dishrouletteapp
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.dishrouletteapp.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
