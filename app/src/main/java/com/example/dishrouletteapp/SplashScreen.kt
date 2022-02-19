7eabe3cd7d53bb20bb43fc3204e0f67578ee2309
George <dolofan_g@yahoo.com>
2022-02-19 13:49:08 +0200

    Splash Screen finish

--- app/src/main/java/com/example/dishrouletteapp/SplashScreen.kt
+++ app/src/main/java/com/example/dishrouletteapp/SplashScreen.kt
@@ -1,6 +1,11 @@
 package com.example.dishrouletteapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -18,8 +23,22 @@ class SplashScreen : AppCompatActivity() {
 
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val logoAnim = AnimationUtils.loadAnimation(this,R.anim.logo_anim)
+        val textAnim = AnimationUtils.loadAnimation(this,R.anim.text_anim)
 
+        val logo = findViewById<ImageView>(R.id.app_logo)
+        val text = findViewById<TextView>(R.id.app_title)
 
+        logo.startAnimation(logoAnim)
+        text.startAnimation(textAnim)
+
+        val splashScreenTimeout = 4000
+        val mainIntent = Intent(this@SplashScreen, MainActivity::class.java)
+
+        Handler().postDelayed({
+            startActivity(mainIntent)
+            finish()
+        }, splashScreenTimeout.toLong())
 
     }
 }
